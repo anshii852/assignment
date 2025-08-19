2a4a9c17fd52312bf9b1aa841ad5a70927196d19
anshii852 <dwivedianshika653@gmail.com>
2025-08-19 10:10:30 +0530

    Create SWAPPING.py

---
+++ SWAPPING.py
@@ -0,0 +1,19 @@
+# Swapping two variables
+
+a = 5
+b = 10
+
+# Before swapping
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swapping using a temporary variable
+temp = a
+a = b
+b = temp
+
+# After swapping
+print("After swapping:")
+print("a =", a)
+print("b =", b)
